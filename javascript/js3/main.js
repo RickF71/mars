2bf55c778772d0c6a6c7c99cafaf23d00b263d3d
Richard Fleischman <rickf71@gmail.com>
2022-03-08 12:44:02 -0600

    lots of change

---
+++ javascript/js3/main.js
@@ -0,0 +1,9 @@
+
+function doStuff() {
+    alert("Doing Stuff!");
+}
+
+function verifyAndSubmit(e) {
+    console.log(document.getElementById('empEntry').submit());
+
+}
---
+++ javascript/test/index.js
@@ -0,0 +1,20 @@
+
+let colors = ['white','black','yellow','blue','red','green'];
+
+let [firstColor,secondColor,...otherColors] = colors;
+
+console.log(firstColor);
+console.log(secondColor);
+console.log(otherColors);
+
+const myClasses = {
+    firstHour: "Ethics",
+    secondHour: "Programming",
+    thirdHour: "Biology"
+}
+let { firstHour, secondHour, thirdHour  } = myClasses;
+
+console.log (secondHour);
+
+
+console.log (typeof(null));
