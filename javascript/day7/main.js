31be3a9a477c7ab1a2068df80d78208ad8d4a77b
Richard Fleischman <rickf71@gmail.com>
2022-03-15 07:01:14 -0500

    more stuff

--- javascript/day7/main.js
+++ javascript/day7/main.js
@@ -1,9 +1,30 @@
 
-function doStuff() {
-    alert("Doing Stuff!");
-}
 
-function verifyAndSubmit(e) {
-    console.log(document.getElementById('empEntry').submit());
+const sales = [ 
+    { item: 'PS4 Pro', stock: 3, original: 399.99 }, 
+    { item: 'Xbox One X', stock: 1, original: 499.99, discount: 0.1 }, 
+    { item: 'Nintendo Switch', stock: 4, original: 299.99 }, 
+    { item: 'PS2 Console', stock: 1, original: 299.99, discount: 0.8 }, 
+    { item: 'Nintendo 64', stock: 2, original: 199.99, discount: 0.65 } 
+    ];
 
+ 
+
+function showSales() {
+    // document.getElementById('consoleSales').innerHTML=sales;
+    Object.values(sales).forEach(val => {
+        // document.getElementById('consoleSales').innerHTML += val.item + '<br />';
+        // document.getElementById('consoleSales').innerHTML += val.stock + '<br />';
+
+        item = val.item;
+        stock=val.stock;
+        original=val.original;
+        discount=val.discount || 0;
+    
+        calcValue = (stock*original) * (1-discount);
+        document.getElementById('consoleSales').innerHTML += stock + ' of ' + item + ' : ($' + original + " ea %" + (discount*100).toFixed(0) + " discount) ";
+        document.getElementById('consoleSales').innerHTML += '$' + calcValue.toFixed(2) + '<br />';
+
+        
+    })
 }
--- javascript/test/index.js
+++ javascript/test/index.js
@@ -1,20 +1,30 @@
 
-let colors = ['white','black','yellow','blue','red','green'];
+// let colors = ['white','black','yellow','blue','red','green'];
 
-let [firstColor,secondColor,...otherColors] = colors;
+// let [firstColor,secondColor,...otherColors] = colors;
 
-console.log(firstColor);
-console.log(secondColor);
-console.log(otherColors);
+// console.log(firstColor);
+// console.log(secondColor);
+// console.log(otherColors);
 
-const myClasses = {
-    firstHour: "Ethics",
-    secondHour: "Programming",
-    thirdHour: "Biology"
-}
-let { firstHour, secondHour, thirdHour  } = myClasses;
+// const myClasses = {
+//     firstHour: "Ethics",
+//     secondHour: "Programming",
+//     thirdHour: "Biology"
+// }
+// let { firstHour, secondHour, thirdHour  } = myClasses;
 
-console.log (secondHour);
+// console.log (secondHour);
 
 
-console.log (typeof(null));
+// console.log (typeof(null));
+
+function getUsers()
+    setTimeout(()=>
+    {
+        cancelIdleCallback([
+            {username: 'John', email:}
+        ])
+    }
+    
+    )
